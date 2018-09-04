a4d2f1a74fa70d4d1f4609f840907df7618f580b
liucz <liuchangzheng25@163.com>
2018-09-04 13:47:43 +0800

    matplotlib 1

---
+++ code3matplotlib/m1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N=50
+x=np.random.rand(N)
+y=np.random.rand(N)
+colors=np.random.rand(N)
+area=np.pi*(15*np.random.rand(N))**2
+
+print(area)
+
+#plt.scatter(x,y,s=area,c=colors,alpha=0.5)
+plt.hist(area)
+
+plt.show()
