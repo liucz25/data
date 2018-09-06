c881387dbc0c1d4667e704370d7012316a56f468
liucz <liuchangzheng25@163.com>
2018-09-06 21:12:37 +0800

    zhengli

--- tiaozhan1/gaishan.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import json
-import sys
-filename="user_study.json"
-with open(filename,'r')as f:
-    datas=json.load(f)
-    for data in datas:
-        print(data["course"])
