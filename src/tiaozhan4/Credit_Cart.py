2be8e4e13d22c3b5b0c0c5edacc71294914f6fe0
liucz <liuchangzheng25@163.com>
2018-09-07 10:59:15 +0800

    tiaozhan4 1

--- src/tiaozhan4/Credit_Cart.py
+++ src/tiaozhan4/Credit_Cart.py
@@ -9,12 +9,18 @@ def test_score(test_feature,test_target):
     data = pd.read_csv("credit_card_train.csv",header=0)
 
 
-    score=model.score(test_feature,test_target)
-
+    #score=model.score(test_feature,test_target)
+    score=9
     return score
 
 if __name__=='__main__':
+    data = pd.read_csv("credit_card_train.csv",header=0)
+    x,y=1,1
+    xx1=data.iloc[:,1:14]
+    xx2=data.iloc[:,15:17]
+    yy=data.iloc[:.:18]
+
     score = test_score(x,y)
 
 print("hello.")
-print(s)
+print(xx2)
