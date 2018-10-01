acc05b31eb1513075d095cd6cf1e1af7b5bc2743
liucz <liuchangzheng25@163.com>
2018-10-01 21:54:15 +0800

    bh-bl to adc?

---
+++ src/python-dicom/b0-b1000-to-acd.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import pydicom
+import numpy as np
+import math
+import matplotlib.pyplot as plt 
+
+
+
+ds0=pydicom.read_file('b0.dcm')  
+ds1=pydicom.read_file('b1000.dcm')
+
+ad=pydicom.read_file('3.dcm')
+
+b0=ds0.pixel_array
+b1=ds1.pixel_array
+
+add=ad.pixel_array
+
+sbl=np.array(b0)
+sbh=np.array(b1)
+adf=np.array(add)
+
+bl=0
+bh=1000
+
+adc=np.log(sbh/sbl)/(bh-bl)
+
+adct=adc*1000
+
+plt.imshow(adc,cmap='gray_r')
+#plt.imshow(adf,cmap='gray_r')
+plt.show()
